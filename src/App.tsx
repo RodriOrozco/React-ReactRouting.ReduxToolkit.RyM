d55ce4cda772a45cdbc4d3ac938ee929f62a3c8a
Rodrigo Orozco <orozcorodri8@gmail.com>
2023-04-10 18:33:40 -0300

    Routes models and Guards implemented in app routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "./App.css";
 import Login from "./pages/Login/Login";
 import Error from "./pages/Error/Error";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import { PrivateRoutes, PublicRoutes } from "./models/routes";
+import AuthGuard from "./guards/auth.guard";
 
 function App(): JSX.Element {
   return (
@@ -12,7 +14,10 @@ function App(): JSX.Element {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="*" element={<Error />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path={PublicRoutes.LOGIN} element={<Login />} />
+          <Route element={<AuthGuard />}>
+            <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { getMorty } from "../../services";
+import { useDispatch } from "react-redux";
+import { createUser } from "../../redux/states/user";
 
 const Login = () => {
+  const login = async () => {
+    const dispatch = useDispatch();
+
+    try {
+      const response = await getMorty();
+      dispatch(createUser(response));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-        <h1>login</h1>
+      <h1>login</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
